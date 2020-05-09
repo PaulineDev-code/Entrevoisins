e92ee7707a92a9bde54932d436f31cca5adfd04d
Pauline <p.cellier91@gmail.com>
2020-05-09 13:04:55 +0200

    bug2

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/detail/DetailActivity.java
@@ -0,0 +1,46 @@
+package com.openclassrooms.entrevoisins.ui.detail;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        long id = getIntent().getLongExtra("id", -1);
+        Neighbour neighbour = DI.getNeighbourApiService().getNeighbourbyid(id);
+        getSupportActionBar().setTitle(neighbour.getName());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        boolean b = super.onSupportNavigateUp();
+        finish();
+        return b;
+    }
+}
